7d37bab34a91bcbd685daefe8786a3fc34d4b32a
= <=>
2025-12-05 19:53:33 +0000

    cors config commit

--- config/cors.php
+++ config/cors.php
@@ -12,11 +12,13 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'login', 'logout', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        'http://academiapro.net',
+        'http://www.academiapro.net',
         'http://localhost:3000',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
